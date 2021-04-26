09ce5330ed25e1361f02980c98b8b3bcddf92976
molykote <eriklim@hotmail.com>
2021-04-26 17:33:24 -0300

    commit-curso-cucumber-linux-26-04-2021

---
+++ CursoCucumber/src/test/java/AprenderCucumber.java
@@ -0,0 +1,25 @@
+import io.cucumber.core.cli.Main;
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+
+@SuppressWarnings("unused")
+public class AprenderCucumber {
+	
+	@Given("^que criei o arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+		
+	}
+	
+	@When("executá-lo")
+	public void executá_lo() {
+	    
+	}
+	
+	@Then("^a especificação deve finalizar com sucesso$")
+	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+		
+	}
+}
